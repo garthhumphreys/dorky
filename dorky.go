7253b0ad4aa0b06be909d17ef8af9fc47220c6a0
Garth Humphreys <garthhumphreys@gmail.com>
2020-06-06 19:05:52 -0500

    initial commit using raw http for scraper

--- dorky.go
+++ dorky.go
@@ -2,46 +2,26 @@ package main
 
 import (
 	"fmt"
-	"github.com/gocolly/colly"
-	// "os"
+	"github.com/PuerkitoBio/goquery"
+	"log"
+	"net/http"
 )
 
 func main() {
-	// sitePtr := flag.String("site", "gov.ky", "a domain")
-	// inurlPtr := flag.String("inurl", "admin", "what to look for in the url")
-	// wildcardPtr := flag.Bool("w", true, "search sub-domains")
-	// resultsPtr := flag.Int("r", 10, "number of dork results to show")
-	//
-	// flag.Parse()
-	// fmt.Println("site:", *sitePtr)
-	// fmt.Println("inurl:", *inurlPtr)
-	// fmt.Println("w:", *wildcardPtr)
-	// fmt.Println("r:", *resultsPtr)
-	// fmt.Println("other params:", flag.Args())
+	resp, err := http.Get("http://go-colly.org/")
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+	fmt.Println("Response status", resp.Status)
 
-	/*
-	Use this for just arguments loose form, instead of flags
-	argsWithProg := os.Args
-	argsWithoutProg := os.Args[1:]
-	arg := os.Args[3]
-	fmt.Println(argsWithProg)
-	fmt.Println(argsWithoutProg)
-	fmt.Println(arg)
-	*/
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	search()
-}
-
-func search()  {
-	fmt.Println("Search")
-	c := colly.NewCollector()
-	c.OnHTML("#menu .computer a", func(element *colly.HTMLElement) {
-		fmt.Printf("title:%s link:%s\n", element.Text, element.Attr("href"))
+	doc.Find("#menu a").Each(func(i int, selection *goquery.Selection) {
+		fmt.Println(selection.Text())
 	})
 
-	c.OnRequest(func(request *colly.Request) {
-		fmt.Println("Visting", request.URL)
-	})
-
-	c.Visit("http://go-colly.org/")
 }
