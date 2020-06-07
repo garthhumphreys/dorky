7c0c4ec5940c583e67faab97815881f1bfd60ec0
Garth Humphreys <garthhumphreys@gmail.com>
2020-06-06 22:49:30 -0500

    refactored code to better handle cli args making the site arg required, also got the go-colly goquery queries working on google

--- dorky.go
+++ dorky.go
@@ -1,23 +1,47 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/gocolly/colly"
+	"os"
+
 	// "os"
 )
 
 func main() {
-	// sitePtr := flag.String("site", "gov.ky", "a domain")
-	// inurlPtr := flag.String("inurl", "admin", "what to look for in the url")
-	// wildcardPtr := flag.Bool("w", true, "search sub-domains")
-	// resultsPtr := flag.Int("r", 10, "number of dork results to show")
-	//
-	// flag.Parse()
-	// fmt.Println("site:", *sitePtr)
-	// fmt.Println("inurl:", *inurlPtr)
-	// fmt.Println("w:", *wildcardPtr)
-	// fmt.Println("r:", *resultsPtr)
-	// fmt.Println("other params:", flag.Args())
+	sitePtr := flag.String("site", "gov.ky", "a domain")
+	inurlPtr := flag.String("inurl", "admin", "what to look for in the url")
+	wildcardPtr := flag.Bool("w", true, "search sub-domains")
+	resultsPtr := flag.Int("r", 10, "number of dork results to show")
+
+	flag.Parse()
+
+	if len(os.Args) < 2 {
+		fmt.Println("expected 'site'")
+		os.Exit(1)
+	}
+
+	if *sitePtr != "" {
+		//search(*sitePtr)
+		fmt.Println("site:", *sitePtr)
+		search("https://www.google.com/search?q=site%3A.ky+inurl%3Aphp")
+	} else {
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	if *inurlPtr != "" {
+		fmt.Println("inurl:", *inurlPtr)
+	}
+
+	if *wildcardPtr {
+		fmt.Println("w:", *wildcardPtr)
+	}
+
+	if *resultsPtr == 10 {
+		fmt.Println("r:", *resultsPtr)
+	}
 
 	/*
 	Use this for just arguments loose form, instead of flags
@@ -29,15 +53,15 @@ func main() {
 	fmt.Println(arg)
 	*/
 
-	search()
+	//search("http://go-colly.org/")
 }
 
-func search()  {
+func search(url string)  {
 	fmt.Println("Search")
 	c := colly.NewCollector(
 		colly.UserAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"),
 		)
-	c.OnHTML("#menu .computer a", func(element *colly.HTMLElement) {
+	c.OnHTML("#search .r > a", func(element *colly.HTMLElement) {
 		fmt.Printf("title:%s link:%s\n", element.Text, element.Attr("href"))
 	})
 
@@ -45,5 +69,5 @@ func search()  {
 		fmt.Println("Visting", request.URL)
 	})
 
-	c.Visit("http://go-colly.org/")
+	c.Visit(url)
 }
